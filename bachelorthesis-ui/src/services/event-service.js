b80b3d40e2f5770e23acb91c7d74f77e315af28e
ManoelaYordanova <manoela.yordanova99@gmail.com>
2023-07-01 13:26:17 +0300

    add

--- bachelorthesis-ui/src/services/event-service.js
+++ bachelorthesis-ui/src/services/event-service.js
@@ -1,9 +1,11 @@
-import axios from 'axios'
+import axios from '../axios'
 
 const API_URL = 'http://localhost:8080/event'
 
 class EventService {
   addEvent (eventRequest) {
+    const accessToken = localStorage.getItem('access_token')
+
     console.log(eventRequest)
     return axios.post(API_URL + '/create',
       {
@@ -12,6 +14,11 @@ class EventService {
         startDateTime: eventRequest.startDateTime,
         endDateTime: eventRequest.endDateTime,
         description: eventRequest.description
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
       })
   }
 
